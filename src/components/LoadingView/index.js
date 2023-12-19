5e1175e549a498244083391795356fd104fee285
V99519030v <vatturiravikrishna@gamail.com>
2023-12-19 05:49:54 +0000

    solution

--- src/components/LoadingView/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css'
-
-const LoaderA = () => (
-  <div className="loading-container">
-    <p>Loading.......</p>
-  </div>
-)
-
-export default LoaderA
--- src/utils/NameContext.js
+++
@@ -1,8 +0,0 @@
-import {createContext} from 'react'
-
-const NameContext = createContext({
-  restaurantName: '',
-  updateRestaurantName: () => {},
-})
-
-export default NameContext
